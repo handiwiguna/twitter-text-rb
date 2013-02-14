8420ea739538225c4e124e7a3f7c224c33d639d4
adhitia.hidayat@gmail.com <adhitia.hidayat@gmail.com>
2013-02-14 13:08:14 +0700

    change tag hash

--- lib/twitter-text/autolink.rb
+++ lib/twitter-text/autolink.rb
@@ -20,9 +20,9 @@ module Twitter
     # Default URL base for auto-linked lists
     DEFAULT_LIST_URL_BASE = "/".freeze
     # Default URL base for auto-linked hashtags
-    DEFAULT_HASHTAG_URL_BASE = "https://twitter.com/#!/search?q=%23".freeze
+    DEFAULT_HASHTAG_URL_BASE = "/tag/".freeze
     # Default URL base for auto-linked cashtags
-    DEFAULT_CASHTAG_URL_BASE = "https://twitter.com/#!/search?q=%24".freeze
+    DEFAULT_CASHTAG_URL_BASE = "/tag/".freeze
 
     # Default attributes for invisible span tag
     DEFAULT_INVISIBLE_TAG_ATTRS = "style='position:absolute;left:-9999px;'".freeze
